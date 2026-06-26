bf483c0b553318704a313fea2f6eef7308bbbfc6
lucas 'aerial tramway' nguyen <l@lucipas.dev>
2026-06-25 20:55:26 -0400

    Tests added

--- test.sh
+++ test.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+
 trap "linedriver -ctx $ctx close" INT # clean up browser when <Ctrl-C> is encountered
 
-geckodriver & 2> /dev/null
+geckodriver > /dev/null 2>&1 &
+killall firefox > /dev/null 2>&1
 
 ctx=$(linedriver) # init webdriver and hold the browser context
 
-linedriver -ctx $ctx navigate "file:///usr/share/doc/" # open UR:
-sleep 10s # wait for the network to settle
+linedriver -ctx "$ctx" navigate "https://www.github.careers/careers-home/jobs?page=1&locations=,,United%20States&categories=Engineering&tags6=Yes" # open UR:
 
-linedriver -ctx $ctx url   # get URL
-linedriver -ctx $ctx title # get title
-linedriver -ctx $ctx src   # get src
+sleep 5s # wait for the network to settle
+
+linedriver -ctx "$ctx" url   # get URL
+linedriver -ctx "$ctx" title # get title
+for i in {0..9}; do 
+	printf "<!DOCTYPE html>\n%s" "$(linedriver -ctx "$ctx" src | jq -r .value | perl -MHTML::Entities -MEncode -pe 'BEGIN { binmode(STDIN, ":utf8"); binmode(STDOUT, ":utf8"); } s/\\n//g; s/\\//g; s/&#39;/"/g; decode_entities($_);')" | xsel -a -t html -x "//*[@id='mat-expansion-panel-header-$i']/span[1]/mat-panel-title/p[2]" - | cut -d " " -f 4
+done
+# s/\\//g
 linedriver -ctx $ctx close
 
 killall geckodriver
+# firefox https://gemini.google.com > /dev/null 2>&1 &
